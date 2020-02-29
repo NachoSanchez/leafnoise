81e5566633768b7333297ad1486908653331210c
NachoSanchez <m.ign.sanchez@gmail.com>
2020-02-29 12:03:28 -0300

    Adding localStorage to Language

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -11,6 +11,16 @@ class LanguageContextProvider extends Component {
         this.setState({english: !this.state.english});
     }
 
+    componentWillMount(){
+        const language =  localStorage.getItem('language');
+        this.setState(JSON.parse(language));
+    }
+ 
+     componentDidUpdate(){
+         const newLang = JSON.stringify(this.state)
+         localStorage.setItem('language', newLang)
+    }
+
     render(){
         return (
             <LanguageContext.Provider value={{...this.state, toggleLanguage: this.toggleLanguage}}>
