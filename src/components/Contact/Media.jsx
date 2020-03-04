5244a66a3d04718925d9c3a9079f6cf24978c941
NachoSanchez <m.ign.sanchez@gmail.com>
2020-03-04 16:22:17 -0300

    fixing media icons

--- src/components/Contact/Media.jsx
+++ src/components/Contact/Media.jsx
@@ -5,8 +5,6 @@ const Media = (props) => {
     return (
         <a 
             href={ props.href } 
-            target="_blank" 
-            rel="noopener noreferrer"
             className={ styles.media }>
             
             <img src={ props.icon } alt={ props.type }/>
